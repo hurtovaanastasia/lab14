928608ba457f701e4f579358611e7223dcc9bac3
anastasia <anastasia@hurto>
2025-03-27 11:18:11 +0300

    добавлен ручной ввод массива

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,13 @@ import java.util.regex.Matcher;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Scanner input1 = new Scanner(System.in);
         System.out.print("ВВедите размерность массива: ");
         int n = input.nextInt();
         String [] adresses = new String[n];
-        System.out.println("Введите элементы массива:");
         for (int i = 0; i < n; i++) {
-            adresses[i] = input.nextLine();
+            System.out.printf("Введите %d элемент массива:", i+1);
+            adresses[i] = input1.nextLine();
         }
         String perfectIP = "^((25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]?\\d)\\.){3}(25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]?\\d)$";
         Pattern pattern = Pattern.compile(perfectIP);
